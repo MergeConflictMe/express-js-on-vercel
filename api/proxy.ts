a76b34b9e6f67f6656f6c592b70c18eabd476197
MergeConflictMe <shukin3937@gmail.com>
2026-06-09 14:49:17 +0500

    Create proxy.ts

---
+++ api/proxy.ts
@@ -0,0 +1,18 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const targetUrl = req.query.url as string
+
+  if (!targetUrl) {
+    return res.status(400).json({ error: 'Use ?url=https://example.com' })
+  }
+
+  fetch(targetUrl)
+    .then(r => r.text())
+    .then(html => {
+      res.setHeader('Content-Type', 'text/html; charset=utf-8')
+      res.setHeader('Access-Control-Allow-Origin', '*')
+      res.send(html)
+    })
+    .catch(e => res.status(500).json({ error: e.message }))
+}
